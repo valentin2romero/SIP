2332f21a0dfe46213ae50811fb864a02bf1fb63d
Vlen2R <71619929+vlen2r@users.noreply.github.com>
2021-08-11 12:29:42 -0300

    Update form.blade.php
    Aplicamos el formatter para acomodar el código, y que permita ser mas legible!

--- resources/views/texto-sentencia/partials/form.blade.php
+++ resources/views/texto-sentencia/partials/form.blade.php
@@ -1,4 +1,4 @@
-<input type="hidden" name="id" value="{{{ isset($texto_sentencia->id) ? $texto_sentencia->id : ''}}}">
+<input type="hidden" name="id" value="{{ isset($texto_sentencia->id) ? $texto_sentencia->id : '' }}">
 
 <div class="row">
 
@@ -9,13 +9,20 @@
                     Variable Representada
                 </div>
                 <div class="cuerpo-2">
-                    Ingrese el nombre de variable correspondiente. Esta variable es la que figura en el <strong>"Modelo"</strong>.<br />
+                    Ingrese el nombre de variable correspondiente. Esta variable es la que figura en el
+                    <strong>"Modelo"</strong>.<br />
                     <strong> Ejemplo:</strong> $Variable<br />
                     <br />
                     <div class="form-group">
                         <div class="form-group">
-                            <input type="text" class="form-control" name="variable" value="{{{ isset($texto_sentencia->variable) ? $texto_sentencia->variable : ''}}}" required autofocus>
-                            @error('variable')<small class="form-text text-danger">{{ $message }}</small>@enderror
+                            <input type="text" class="form-control" name="variable"
+                                value="{{ isset($texto_sentencia->variable) ? $texto_sentencia->variable : '' }}"
+                                required autofocus>
+                            @error('variable')
+                                <small class="form-text text-danger">
+                                    {{ $message }}
+                                </small>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -37,8 +44,14 @@
                             <br />
                             <div class="form-group">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" name="opcion" value="{{{ isset($texto_sentencia->opcion) ? $texto_sentencia->opcion : ''}}}" required autofocus>
-                                    @error('opcion')<small class="form-text text-danger">{{ $message }}</small>@enderror
+                                    <input type="text" class="form-control" name="opcion"
+                                        value="{{ isset($texto_sentencia->opcion) ? $texto_sentencia->opcion : '' }}"
+                                        required autofocus>
+                                    @error('opcion')
+                                        <small class="form-text text-danger">
+                                            {{ $message }}
+                                        </small>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -94,8 +107,14 @@
                     Es la fecha en la cual comienza a estar vigente la <u>descripcion.</u><br />
                     <br />
                     <div class="form-group">
-                        <input type="date" class="form-control" name="fecha_inicio" value="{{{ isset($texto_sentencia->fecha_inicio) ? $texto_sentencia->fecha_inicio : ''}}}" required autofocus>
-                        @error('fecha_inicio')<small class="form-text text-danger">{{ $message }}</small>@enderror
+                        <input type="date" class="form-control" name="fecha_inicio"
+                            value="{{ isset($texto_sentencia->fecha_inicio) ? $texto_sentencia->fecha_inicio : '' }}"
+                            required autofocus>
+                        @error('fecha_inicio')
+                            <small class="form-text text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -113,8 +132,14 @@
                     Recuerde que se considera este ultimo dia como vigente.<br />
                     <br />
                     <div class="form-group">
-                        <input type="date" class="form-control" name="fecha_final" value="{{{ isset($texto_sentencia->fecha_final) ? $texto_sentencia->fecha_final : ''}}}" required autofocus>
-                        @error('fecha_final')<small class="form-text text-danger">{{ $message }}</small>@enderror
+                        <input type="date" class="form-control" name="fecha_final"
+                            value="{{ isset($texto_sentencia->fecha_final) ? $texto_sentencia->fecha_final : '' }}"
+                            required autofocus>
+                        @error('fecha_final')
+                            <small class="form-text text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -131,12 +156,18 @@
             Descripcion
         </div>
         <div class="cuerpo-2">
-            Ingrese la descripcion correspondiente. Este valor sera por el cual se remplaze la variable anteriormente mencionada.<br />
+            Ingrese la descripcion correspondiente. Este valor sera por el cual se remplaze la variable anteriormente
+            mencionada.<br />
             <br />
             <div class="form-group">
                 <div class="form-group">
-                    <textarea class="form-control" name="descripcion" required autofocus>{{ isset($texto_sentencia->descripcion) ? $texto_sentencia->descripcion : ''}}</textarea>
-                    @error('descripcion')<small class="form-text text-danger">{{ $message }}</small>@enderror
+                    <textarea class="form-control" name="descripcion" required
+                        autofocus>{{ isset($texto_sentencia->descripcion) ? $texto_sentencia->descripcion : '' }}</textarea>
+                    @error('descripcion')
+                        <small class="form-text text-danger">
+                            {{ $message }}
+                        </small>
+                    @enderror
                 </div>
             </div>
         </div>
@@ -152,14 +183,20 @@
         </div>
         <div class="cuerpo-2">
             Elija la dependencia a cual pertenece este texto sentencia.<br />
-            En caso de utilizar este texto sentencia para todas las dependencias, elegir dicha opcion. En dicho caso, este texto sentencia se utilizara en todas las dependencias, en los cuales no se haya asignado esta misma variable y opcion (para dicha dependencia) o esten fuera del rango de fechas.<br/>
+            En caso de utilizar este texto sentencia para todas las dependencias, elegir dicha opcion. En dicho caso,
+            este texto sentencia se utilizara en todas las dependencias, en los cuales no se haya asignado esta misma
+            variable y opcion (para dicha dependencia) o esten fuera del rango de fechas.<br />
             <br />
             <div class="form-group">
                 <div class="form-group">
                     <select class="form-control" name="dependencia_id" autofocus>
-                        <option value="" {{{(isset($texto_sentencia->dependencia_id) and is_null($texto_sentencia->dependencia_id)) ? 'selected' : ''}}}>Todas las dependencias</option>
+                        <option value=""
+                            {{ (isset($texto_sentencia->dependencia_id) and is_null($texto_sentencia->dependencia_id)) ? 'selected' : '' }}>
+                            Todas las dependencias</option>
                         @foreach ($dependencias as $dependencia)
-                            <option value="{{ $dependencia->id }}" {{{(isset($texto_sentencia->dependencia_id) and ($texto_sentencia->dependencia_id == $dependencia->id)) ? 'selected' : ''}}}>{{ $dependencia->descripcion }}</option>
+                            <option value="{{ $dependencia->id }}"
+                                {{ (isset($texto_sentencia->dependencia_id) and $texto_sentencia->dependencia_id == $dependencia->id) ? 'selected' : '' }}>
+                                {{ $dependencia->descripcion }}</option>
                         @endforeach
                     </select>
                     @error('dependencia_id')
